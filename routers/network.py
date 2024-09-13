5ff0c69e2e75aca7fe76b29bbc0f1fb826ac759d
Samyak Sharma <sammi.bgas@gmail.com>
2024-09-14 01:04:38 +0530

    output changes

--- routers/network.py
+++ routers/network.py
@@ -6,42 +6,66 @@ def fetch_network_data():
     db = database.get_connection()
     cursor = db.cursor()
     try:
+        # Fetching node and interface details
         get_nodes_query = """
-            SELECT 
-                n.host,
-                json_agg(
-                    json_build_object(
-                        'interface', i.interface,
-                        'ip', i.ip
-                    )
-                ) AS interfaces
+            SELECT n.id AS node_id, n.host, i.interface, i.ip
             FROM nodes n
             JOIN interfaces i ON n.id = i.node_id
-            GROUP BY n.host;
+            ORDER BY n.id;
         """
-
         cursor.execute(get_nodes_query)
         nodes_pre = cursor.fetchall()
 
+        # Fetching connections data
         get_connections_query = """
-            SELECT * FROM connections
+            SELECT id, ip1, ip2 FROM connections;
         """
-
         cursor.execute(get_connections_query)
-        connections = cursor.fetchall()
+        connections_pre = cursor.fetchall()
 
         print("Fetched network data successfully.")
 
-        nodes = []
+        # Dictionary to group interfaces by node
+        nodes_dict = {}
 
-        print(f'Nodes: {nodes_pre}')
+        # Process node/interface data
+        for row in nodes_pre:
+            node_id, host, interface, ip = row
+            if node_id not in nodes_dict:
+                nodes_dict[node_id] = {
+                    "id": node_id,
+                    "type": "pc",  # Assuming type is "pc" for all, adjust if necessary
+                    "name": host,  # Host as the name of the node
+                    "interfaces": []
+                }
+
+            # Append each interface to the corresponding node
+            nodes_dict[node_id]["interfaces"].append({
+                "ip": ip,
+                "name": interface
+            })
+
+        # Convert dictionary to list of nodes
+        nodes = list(nodes_dict.values())
+
+        # Process connections data and format into JSON
+        connections = []
+        for conn in connections_pre:
+            conn_id, ip1, ip2 = conn
+            connections.append({
+                "id": conn_id,
+                "ip1": ip1,
+                "ip2": ip2
+            })
+
+        # For debugging purposes
+        print(f'Nodes: {nodes}')
         print(f'Connections: {connections}')
 
+        # Return the structured data
         return {
             "nodes": nodes,
             "connections": connections
         }
     finally:
         cursor.close()
-
-# fetch_network_data()
